fbfee56f8a894a9e2e28fd9b4c10b3fff3174824
Hackagon <phonghiavan@gmail.com>
2020-11-07 20:45:55 +0700

    homework 1

---
+++ homework-01.py
@@ -0,0 +1,42 @@
+"""
+Homework Assignment #1: Variables
+Listing the metadata of "When You Tell Me That You Love Me" Song
+"""
+
+"""
+Part 1: Define variables
+"""
+
+# Favorite song
+# Song name
+my_favorite_song = "When You Tell Me That You Love Me"
+
+# Video link
+video_link = "https://www.youtube.com/watch?v=MwSL-57pNqM"
+
+# Duration (minutes)
+duration = 4
+
+# singers
+singers = ["Westlife", "Diana Ross"]
+
+# Youtube views
+youtube_views = 61704058
+
+# Release Date
+release_date = "Oct 4, 2009"
+
+# Genre
+genre = "love"
+
+"""
+Part 2: Print variables
+"""
+print(my_favorite_song)
+print(video_link)
+print(duration)
+print(singers)
+print(youtube_views)
+print(release_date)
+print(genre)
+
